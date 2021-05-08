92f2cd1aef1da94c8bf95daa5f4ed7b548bedb8b
SilverFlame83 <76583502+SilverFlame83@users.noreply.github.com>
2021-05-08 11:17:41 +0300

    Add files via upload

---
+++ 08. Lab-Classes/01.Person.js
@@ -0,0 +1,14 @@
+class Person{
+    constructor(firstName,lastName,age,email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    toString(){
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+    }
+}
+
+const p = new Person();
+console.log(p instanceof Person);
